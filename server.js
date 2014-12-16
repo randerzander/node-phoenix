5ef5c600e32d4c2c0503153e366544350bd74b1f
Randy Gelhausen <rgelhau@gmail.com>
2014-12-16 02:55:39 -0500

    updated jdbc driver location for HDP 2.2

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var jdbc = new (require('jdbc'));
 
 var config = {
-  libpath: '/usr/lib/phoenix/phoenix-client.jar',
+  libpath: '/usr/hdp/current/phoenix-client/phoenix-client.jar',
   drivername: 'org.apache.phoenix.jdbc.PhoenixDriver',
   url: 'jdbc:phoenix:' + process.argv[2] //example: localhost:2181:/hbase-unsecure
 }
@@ -11,9 +11,14 @@ jdbc.initialize(config, function(err, res){ if (err){ console.log(err); } });
 
 jdbc.open(function(err, conn) {
   if (conn) {
+    //Run first query
     jdbc.executeQuery('SELECT * FROM web_stat', function(err, results){
       if (err){ console.log(err); }
       else if (results) { console.log(results); }
+    });
+    //Run second query
+    jdbc.executeQuery('select count(*) from web_stat', function(err, results){
+      console.log(results);
     });
   }else{
     console.log('Connection object:' + conn);
